c0d2e29e44f29683e2a9957ac9d4d8cafdfe112b
bdanning <bernhard.danninger@fh-salzburg.ac.at>
2021-07-01 15:31:33 +0200

    run CommandLineAppStartupRunner as one db transaction

--- src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
+++ src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
@@ -5,8 +5,10 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import at.ac.fhsalzburg.swd.spring.dao.Customer;
+import at.ac.fhsalzburg.swd.spring.dao.CustomerRepository;
 import at.ac.fhsalzburg.swd.spring.services.CustomerServiceInterface;
 import at.ac.fhsalzburg.swd.spring.services.OrderServiceInterface;
 import at.ac.fhsalzburg.swd.spring.services.ProductServiceInterface;
@@ -21,16 +23,21 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     
     @Autowired
     OrderServiceInterface orderService;
+    
+    @Autowired
+    CustomerRepository repo;
 
 
    // Initialize System with preset accounts and stocks
     @Override
+    @Transactional // this method runs within one database transaction; performing a commit at the end
     public void run(String...args) throws Exception {
 
     	customerService.addCustomer("Max", "Mustermann", "max@muster.man", "123");
     	productService.addProduct("first product", 3.30f);
     	Customer customer = customerService.getAll().iterator().next();
     	customer.setCredit(100l);
+    	customer = customerService.getById(1l);
     	orderService.addOrder(new Date(),customer, productService.getAll());
     }
 }
