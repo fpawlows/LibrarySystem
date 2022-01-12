d4a572f58c98b57e29214932e294d92e5f2d2934
Bernhard Danninger <bernhard.danninger@fh-salzburg.ac.at>
2022-01-12 14:44:33 +0100

    unit testing session attributes

--- src/test/java/at/ac/fhsalzburg/swd/spring/test/ControllerTest.java
+++ src/test/java/at/ac/fhsalzburg/swd/spring/test/ControllerTest.java
@@ -5,18 +5,26 @@ import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.core.Is.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.internal.matchers.Equals;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,6 +45,9 @@ public class ControllerTest {
 	
     @Autowired
     private MockMvc mvc;
+    
+    @Autowired
+    private WebApplicationContext webApplicationContext;
  	
     // in these tests we focus on the controller, so we don't test the repo and mock the needed behavior
     @MockBean
@@ -44,6 +55,14 @@ public class ControllerTest {
     
     @Autowired
     MyController myController;
+    
+    @BeforeEach //this method will be run before each test
+    public void setup()
+    {
+    	//init webcontext
+    	this.mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    
  
     @Test
     public void whenControllerInjected_thenNotNull() throws Exception {
@@ -54,12 +73,18 @@ public class ControllerTest {
     @Test
     public void givenNothing_whenHome_thenIndex()
   	      throws Exception {
+    	
+    	MockHttpSession session = new MockHttpSession(); // set session attribute (mocked session)
+    	session.setAttribute("count", 3);
+    	    
     
     	mvc.perform(MockMvcRequestBuilders.get("/")
+    			.session(session) // set the mocked session
         		.contentType(MediaType.TEXT_HTML))
         		.andExpect(status().isOk())
         		.andExpect(model().attributeExists("customers"))
-        		.andExpect(view().name("index"));;
+        		.andExpect(view().name("index"))        		
+        		.andExpect(request().sessionAttribute("count", is(4))); //check if session attribute has changed properly
     	
     }
     
