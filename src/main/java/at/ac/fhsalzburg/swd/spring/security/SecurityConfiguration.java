3b6d24c191c147002c25ac3151a9f94fc6d9d4c3
filip <filippawlowski01@wp.pl>
2023-01-12 16:22:51 +0100

    WV. Some errors with serialization.

--- src/main/java/at/ac/fhsalzburg/swd/spring/security/SecurityConfiguration.java
+++ src/main/java/at/ac/fhsalzburg/swd/spring/security/SecurityConfiguration.java
@@ -9,11 +9,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
-	
-	
+
+
 	@Autowired
 	private TokenService tokenService;
-	
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// order is important, start with most specific
@@ -22,29 +22,29 @@ public class SecurityConfiguration {
 		.addFilterBefore(new JwtAuthenticationFilter(tokenService),
 				UsernamePasswordAuthenticationFilter.class)
 	    .authorizeRequests()
-	    	.antMatchers("/api/**") 
+	    	.antMatchers("/api/**")
 			.hasAnyRole("USER", "ADMIN") // everything under /api needs either user or admin role
 	    	.antMatchers("/admin/**")
 			.hasAnyRole("ADMIN") // everything under /admin needs admin role
 			.antMatchers("/user/**")
-			.hasAnyRole("USER", "ADMIN") // everything under /user needs either user or admin role		
+			.hasAnyRole("USER", "ADMIN") // everything under /user needs either user or admin role
 	    .and().csrf().disable().formLogin() // add form based authentication
 	    	.loginPage("/login") // our login page
-	    	.defaultSuccessUrl("/") // where to go when login was successful
+	    	.defaultSuccessUrl("/home") // where to go when login was successful
 	    	.failureUrl("/login-error")
-	    .and().csrf().disable().logout()    	
-    		.logoutSuccessUrl("/") // where to go after logout
+	    .and().csrf().disable().logout()
+    		.logoutSuccessUrl("/home") // where to go after logout
     		.invalidateHttpSession(true) // at logout invalidate session
     		.deleteCookies("JSESSIONID") // and delete session cookie
       	;
-		
+
 		http.headers().frameOptions().disable(); // h2-console would not work otherwise
-		
-	    return http.build();		
-		
+
+	    return http.build();
+
 	}
-	
-	
-	
-	
+
+
+
+
 }
