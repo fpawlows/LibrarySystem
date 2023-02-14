04699fa7893bfbc3835a79d7c38883a55fd00d92
Christoph Wagner <cwagner.its-b2020@fh-salzburg.ac.at>
2023-02-14 18:07:53 +0100

    commandlineloader

--- src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
+++ src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
@@ -11,8 +11,11 @@ import java.util.random.RandomGenerator;
 import at.ac.fhsalzburg.swd.spring.model.*;
 import at.ac.fhsalzburg.swd.spring.model.ids.CompartmentId;
 import at.ac.fhsalzburg.swd.spring.model.ids.ShelfId;
+import at.ac.fhsalzburg.swd.spring.model.medias.Audio;
 import at.ac.fhsalzburg.swd.spring.model.medias.Book;
 import at.ac.fhsalzburg.swd.spring.model.medias.Media;
+import at.ac.fhsalzburg.swd.spring.model.medias.Movie;
+import at.ac.fhsalzburg.swd.spring.model.medias.Paper;
 import at.ac.fhsalzburg.swd.spring.services.MediaServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -48,12 +51,16 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
                    // end
     public void run(String... args) throws Exception {
 
-    	if (userService.getByUsername("admin")!=null) return; // data already exists -> return
-
+        
         Calendar c = Calendar.getInstance();
         c.setTime(new Date());
         c.add(Calendar.YEAR, -100);
+        
+    	if (userService.getByUsername("admin")!=null) return; // data already exists -> return
     	userService.addUser("admin", "Administrator", "admin@work.org", "123", c.getTime(), "admin","ADMIN");
+
+        if (userService.getByUsername("Max")!=null) return; // data already exists -> return
+    	userService.addUser("Max", "Mustermann", "max@work.org", "123", new Date(), "max","USER");
 
         productService.addProduct("first product", 3.30f);
         User user = userService.getAll().iterator().next();
@@ -62,7 +69,8 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
         orderService.addOrder(new Date(), user, productService.getAll());
 
 
-        final Integer N_MEDIAS = 40;
+        final Integer N_MEDIAS = 100;
+        final Integer N_MEDIAS_P_TYPE = N_MEDIAS/4;
         final Integer N_FIRST_MEDIAS_FOR_QUEUES = 3;
 
         //TODO change below to setting using services
@@ -77,14 +85,33 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 
         String name = "Library";
 
-        for (int i = 0; i < N_MEDIAS; i++) {
-            givenGenres.add(new Genre("Horror" + i));
-            mediaService.addGenre(givenGenres.get(i));
-            givenMedias.add(new Book("Twillight" + i, "Byla sobie zabka mala rerekumum", 12, i, Integer.toString(i)));
-            givenMedias.get(i).setGenres(givenGenres);
-            //Reservation reservation = new Reservation(new ReservationId(givenMedias.get(i % N_FIRST_MEDIAS_FOR_QUEUES), givenUsers.get(i)), (int) Math.floor((i + N_FIRST_MEDIAS_FOR_QUEUES) / N_FIRST_MEDIAS_FOR_QUEUES));
-            //givenReservations.get(i % N_FIRST_MEDIAS_FOR_QUEUES).add(reservation);
+        for (int i = 0; i < N_FIRST_MEDIAS_FOR_QUEUES; i++) {
+            givenReservations.add(new ArrayList<Reservation>());
+        }
+        
+        givenGenres.add(new Genre("Documentation"));
+        givenGenres.add(new Genre("Horror"));
+        givenGenres.add(new Genre("Family"));
+        givenGenres.add(new Genre("Thriller"));
+        givenGenres.add(new Genre("Romantic"));
+        givenGenres.add(new Genre("Dokumentation"));
+        
+        for (var g : givenGenres)
+        	mediaService.addGenre(g);
 
+        for (int i = 0; i < N_MEDIAS; i++) {       	
+            if(i < N_MEDIAS_P_TYPE)
+                givenMedias.add(new Book("BookXY " + i, "Description" + i, (i%9*3)%19, Integer.MAX_VALUE-(i*3), "AuthorXY " + i));
+            else if(i < 2 * N_MEDIAS_P_TYPE)
+                givenMedias.add(new Audio("Audio " + i, "Description" + i, (i%9*3)%19, "MP3", i*4));
+            else if(i < 3 * N_MEDIAS_P_TYPE)
+                givenMedias.add(new Movie("MovieXY " + i, "Description" + i, (i%9*3)%19, i*4, "MP4"));
+            else if(i < 4 * N_MEDIAS_P_TYPE)
+                givenMedias.add(new Paper("PaperXY " + i, "Description" + i, (i%9*3)%19, i));
+            
+            int g = i%3;
+            givenMedias.get(i).setGenres(givenGenres.subList(g, g+3));
+            
             mediaService.addMedia(givenMedias.get(i));
 
             name = "Library"+i;
@@ -111,7 +138,43 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
                     }
                 }
             }
-            //reservationService.persist(reservation);
         }
+
+        // for (int i = 0; i < N_MEDIAS; i++) {
+        //     //givenGenres.add(new Genre("Horror" + i));
+        //     mediaService.addGenre(givenGenres.get(i));
+        //     givenMedias.add(new Book("Twillight" + i, "Byla sobie zabka mala rerekumum", 12, i, Integer.toString(i)));
+        //     givenMedias.get(i).setGenres(givenGenres);
+        //     //Reservation reservation = new Reservation(new ReservationId(givenMedias.get(i % N_FIRST_MEDIAS_FOR_QUEUES), givenUsers.get(i)), (int) Math.floor((i + N_FIRST_MEDIAS_FOR_QUEUES) / N_FIRST_MEDIAS_FOR_QUEUES));
+        //     //givenReservations.get(i % N_FIRST_MEDIAS_FOR_QUEUES).add(reservation);
+
+        //     mediaService.addMedia(givenMedias.get(i));
+
+        //     name = "Library"+i;
+        //     if(!mediaService.addLocation(name)) {
+        //         throw new ExceptionInInitializerError();
+        //     }
+        //     for (int j=0; j<6; j++) {
+        //         Location location = mediaService.getLocationByName(name).stream().iterator().next();
+        //         if(!mediaService.addShelf(j, location.getId())) {
+        //             throw new ExceptionInInitializerError();
+        //         }
+
+        //         for (int k=0; k<6; k++) {
+        //             ShelfId shelfId = new ShelfId(j, location);
+        //             if(!mediaService.addCompartment(k+j+1, k, shelfId)) {
+        //                 throw new ExceptionInInitializerError();
+        //             }
+
+        //             CompartmentId compartmentId = new CompartmentId(k, mediaService.getShelfById(shelfId));
+        //             Compartment compartment = mediaService.getCompartmentById( compartmentId);
+        //             if(!mediaService.addCopy(givenMedias.get(i), compartment, RandomGenerator.getDefault().nextInt(213454))){
+        //                 //throw new PersistenceException("Couldnt add copy");
+        //             }
+        //             }
+        //         }
+        //     }
+        //     //reservationService.persist(reservation);
+        // }
     }
 
